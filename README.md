4f337dcb1f54c1864c53ffb4ab654d9da99a27e0
baba-s <shotababa1206@gmail.com>
2022-08-27 19:40:47 +0900

    update

--- README.md
+++ README.md
@@ -4,16 +4,9 @@ Vector2Int や Vector3Int を Vector2 や Vector3 などに変換する拡張メ
 
 ## 使用例
 
-### Vector2Int
-
 ```cs
-Vector3 vector3 = Vector2Int.one.ToVector3();
-Vector4 vector4 = Vector2Int.one.ToVector4();
-```
-
-### Vector3Int
-
-```cs
-Vector2 vector2 = Vector3Int.one.ToVector2();
-Vector4 vector4 = Vector3Int.one.ToVector4();
+var v1 = Vector2.one + Vector3.one.ToVector2();
+var v2 = Vector2.one + Vector3Int.one.ToVector2();
+var v3 = Vector3.one + Vector2.one.ToVector3();
+var v4 = Vector3.one + Vector2Int.one.ToVector3();
 ```
--- Runtime/VectorConvertExtensionMethods.cs
+++ Runtime/VectorConvertExtensionMethods.cs
@@ -1,27 +1,27 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Kogane
 {
     public static class VectorConvertExtensionMethods
     {
-        public static Vector3 ToVector3( this Vector2Int self )
+        public static Vector3 ToVector3( this Vector2 self )
         {
-            return new Vector3( self.x, self.y );
+            return new( self.x, self.y );
         }
 
-        public static Vector4 ToVector4( this Vector2Int self )
+        public static Vector3 ToVector3( this Vector2Int self )
         {
-            return new Vector4( self.x, self.y, 0, 0 );
+            return new( self.x, self.y );
+        }
+
+        public static Vector2 ToVector2( this Vector3 self )
+        {
+            return new( self.x, self.y );
         }
 
         public static Vector2 ToVector2( this Vector3Int self )
         {
-            return new Vector2( self.x, self.y );
-        }
-
-        public static Vector4 ToVector4( this Vector3Int self )
-        {
-            return new Vector4( self.x, self.y, 0, 0 );
+            return new( self.x, self.y );
         }
     }
 }
