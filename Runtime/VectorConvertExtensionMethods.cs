b3b07087fbcbbaca167a19b07e8487689edbfe7e
baba-s <shotababa1206@gmail.com>
2023-12-30 14:39:22 +0900

    add

--- Runtime/VectorConvertExtensionMethods.cs
+++ Runtime/VectorConvertExtensionMethods.cs
@@ -14,12 +14,27 @@ namespace Kogane
             return new( self.x, self.y );
         }
 
+        public static Vector3Int ToVector3Int( this Vector2Int self )
+        {
+            return new( self.x, self.y, 0 );
+        }
+
         public static Vector2 ToVector2( this Vector3 self )
         {
             return new( self.x, self.y );
         }
 
         public static Vector2 ToVector2( this Vector3Int self )
+        {
+            return new( self.x, self.y );
+        }
+
+        public static Vector2Int ToVector2Int( this Vector3 self )
+        {
+            return new( ( int )self.x, ( int )self.y );
+        }
+
+        public static Vector2Int ToVector2Int( this Vector3Int self )
         {
             return new( self.x, self.y );
         }
